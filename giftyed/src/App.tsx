5ac483c9e75546d108ce78eba391de78f328ec42
Sebastian Wiora <sebastian.wiora99@gmail.com>
2023-12-07 01:35:51 +0100

    Fix register and login auth

--- giftyed/src/App.tsx
+++ giftyed/src/App.tsx
@@ -4,11 +4,11 @@ import HomePage from "./pages/home-page/HomePage";
 import LoginPage from "./pages/login-page/LoginPage";
 import RegisterPage from "./pages/register-page/RegisterPage";
 import React from "react";
-import SignIn from "./pages/sign-in/SignIn";
 import AdminPage from "./pages/admin-page/AdminPage";
 import FollowPage from "./pages/follow-page/FollowPage";
 import SearchPage from "./pages/search-page/SearchPage";
 import ProfilePage from "./pages/profile-page/ProfilePage";
+import SignUpButton from "./components/singUpButton/SignUpButton";
 
 
 const App:React.FC = () => {
@@ -18,11 +18,11 @@ const App:React.FC = () => {
         <Route index element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register" element={<RegisterPage/>}/>
-        <Route path="/signIn" element={<SignIn/>}/>
         <Route path="/admin" element={<AdminPage/>}/>
         <Route path="/follow" element={<FollowPage/>}/>
         <Route path="/search" element={<SearchPage/>}/>
         <Route path="/profile" element={<ProfilePage/>}/>
+        <Route path="/signup" element={<SignUpButton/>}/>
       </Routes>
     </BrowserRouter>
   )
